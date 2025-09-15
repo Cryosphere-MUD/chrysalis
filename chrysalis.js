d1ac196326a9fb05b25dac023cf845989799d2d0
Cryosphere Developers <cryosphere@cryosphere.org>
2025-09-15 16:53:54 +0000

    better <title> and mention host and port in connection message

--- chrysalis.js
+++ chrysalis.js
@@ -4,6 +4,8 @@ import { socketConnect } from "./socket.js";
 
 import { injectText, renderOutputData, resetANSIState, scrollToEnd } from "./terminal.js";
 
+import { mudhost, mudport, conn_title, disconn_title } from "./settings.js";
+
 import { paste, keyDown, resetCommand } from "./command.js";
 
 const main = document.getElementById("main");
@@ -31,8 +33,9 @@ updateSize();
 let connected = false;
 
 function handleConnect(e) {
+  document.title = conn_title;
   connected = true;
-  injectText(["/// connected to remote server"]);
+  injectText(["/// connected to " + mudhost + " " + mudport]);
   resetCommand();
   command.style.display = "inline";
   reconnect.style.display = "none";
@@ -40,6 +43,7 @@ function handleConnect(e) {
 }
 
 function handleDisconnect(e) {
+  document.title = disconn_title;
   connected = false;
   injectText(["/// connection closed by remote server"]);
   command.style.display = "none";
--- settings.js
+++ settings.js
@@ -1,3 +1,9 @@
 export const url = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + ":4443/ws/";
+
+export const mudhost = "cryosphere.org";
+export const mudport = 6666;
+export const conn_title = "Cryosphere [Connected]";
+export const disconn_title = "Cryosphere [Disconnected]";
+
 import { handleTable } from "./cryosphere.js";
 export { handleTable };
